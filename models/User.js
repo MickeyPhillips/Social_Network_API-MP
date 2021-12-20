6bef1d307ced604476601424a62c896995fb7669
Mickey Phillips <phillipsmickey41@gmail.com>
2021-12-20 15:01:28 -0600

    added dateFormat folder, added ReactioSchema and updated the thought model

--- models/User.js
+++ models/User.js
@@ -1,22 +1,43 @@
 const { Schema, model } = require('mongoose')
 
-const UserSchema = new Schema({
-    username: {
-        type: String,
-        unique: true,
-        required: 'Username is required',
-        trim: true
+const UserSchema = new Schema(
+    {
+        username: {
+            type: String,
+            unique: true,
+            required: 'Username is required',
+            trim: true
+        },
+        email: {
+            type: String,
+            unique: true,
+            required: 'Email is required',
+            match: [/.+@.+\..+/, 'Please enter a valid e-mail address']
+        },
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Thought'
+            }
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'User'
+            }
+        ]
     },
-    email: {
-        type: String,
-        unique: true,
-        required: 'Email is required',
-        match: [/.+@.+\..+/, 'Please enter a valid e-mail address']
-    },
-    // Reference 'Thoughts'
-    // Reference 'Friends'
-})
+    {
+        toJSON: {
+            virtuals: true
+        },
+        id: false
+    }
+)
 
+UserSchema.virtual('friendCount').get(function() {
+    return this.friends.length
+})
 
 const User = model('User', UserSchema)
 
