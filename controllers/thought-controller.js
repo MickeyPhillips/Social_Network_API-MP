0cb093650a170e57aaa30b8f414a2c6e3aa92991
Mickey Phillips <phillipsmickey41@gmail.com>
2021-12-21 15:34:14 -0600

    finished reactions

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -75,11 +75,41 @@ const thoughtController = {
                 res.status(400).json(err)
             })
     },
-    addReaction() {
-
+    addReaction({ params, body }, res) {
+        Thought.findOneAndUpdate(
+            { _id: params.thoughtId },
+            { $push: { reactions: body } },
+            { new: true, runValidators: true }
+        )
+            .then(dbThoughtData => {
+                if(!dbThoughtData) {
+                    res.status(404).json({ message: 'No thought found with this id!' })
+                    return
+                }
+                res.json(dbThoughtData)
+            })
+            .catch(err => {
+                console.log(err)
+                res.status(400).json(err)
+            })
     },
-    removeReaction() {
-
+    removeReaction({ params }, res) {
+        Thought.findOneAndUpdate(
+            { _id: params.thoughtId },
+            { $pull: { reactions: { reactionId: params.reactionId } } },
+            { new: true }
+        )
+            .then(dbThoughtData => {
+                if(!dbThoughtData) {
+                    res.status(404).json({ message: 'No thought found with this id!' })
+                    return
+                }
+                res.json(dbThoughtData)
+            })
+            .catch(err => {
+                console.log(err)
+                res.status(400).json(err)
+            })
     }
 }
 
